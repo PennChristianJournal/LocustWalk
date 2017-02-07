ab121e4a4e0f4d6d9e55afd4ef01b94f7f2a04a7
Austin Eng <213reeses@gmail.com>
2017-02-06 21:52:39 -0500

    admin panel header

--- src/react/views/admin/index.js
+++ src/react/views/admin/index.js
@@ -1,12 +1,16 @@
 
 import React from 'react'
+import AdminLayout from './templates/admin-layout'
 
-const AdminHome = () => 
-    <div>
-        This is the admin panel
-    </div>
+const AdminHome = () => (
+    <AdminLayout id="admin-page">
+        <div>This is the admin panel</div>
+    </AdminLayout>
+)
 
 export default AdminHome
+
+AdminHome.metadata = Object.assign({}, AdminLayout.metadata)
 
 import {mount} from '../../helpers/page'
 mount(AdminHome)
---
+++ src/react/views/admin/templates/admin-layout.js
@@ -0,0 +1,22 @@
+
+import React, {Component} from 'react'
+import Navbar from '../components/navbar'
+
+class AdminLayout extends Component {
+    render() {
+        return (
+            <div className="admin-layout" id={this.props.id}>
+                <Navbar />
+                <div className="container">
+                    {this.props.children}
+                </div>
+                <footer>
+                    <script src="/bower_components/jquery/dist/jquery.min.js"></script>
+                    <script src="/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+                </footer>
+            </div>
+        )
+    }
+}
+
+export default AdminLayout
