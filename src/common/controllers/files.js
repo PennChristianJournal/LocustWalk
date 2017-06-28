de328794c7aa6a36e84271452ecd0fe66dad62de
Austin Eng <213reeses@gmail.com>
2017-06-27 21:12:31 -0400

    image attachments and incremental deprecation of GridFS

--- src/common/controllers/files.js
+++ src/common/controllers/files.js
@@ -3,30 +3,43 @@ import { Router } from 'express';
 const router = new Router();
 
 import File from '../models/file';
+import SmallFile from '../models/smallFile';
 import mkdirp from 'mkdirp';
 import fs from 'fs';
 
 const fileRoot = `${__dirname}/../../../public/files`;
 router.get('/:id', function(req, res, next) {
-  var assetPath = `${fileRoot}/${req.params.id}`;
-  fs.exists(assetPath, (exists) => {
-    if (exists) {
-      next();
+
+  SmallFile.findOne({
+    _id: req.params.id
+  }, function(err, file) {
+    if (err) return next(err);
+    if (file) {
+      // If a file is stored with the old schema, remove it
+      File.remove({_id: req.params.id}, (err) => {});
+      
+      res.contentType(file.contentType);
+      res.send(file.data);
     } else {
+
       File.findOne({_id: req.params.id}, function(err, file, stream) {
-        if (err) {
-          console.log(err);
-        }
+        if (err) return next(err);
         if (file) {
-          process.nextTick(function() {
-            mkdirp(fileRoot, function(err) {
-              if (err) {
-                console.log(err);
-              }
-              var wstream = fs.createWriteStream(assetPath);
-              stream.pipe(wstream);
+          
+          // incrementally remove GridFS
+          let data = new Buffer('');
+          stream.on('data', function(chunk) {
+            data = Buffer.concat([data, chunk]);
+          });
+
+          stream.on('end', function() {
+            SmallFile.create({
+              _id: file._id,
+              data,
+              contentType: file.contentType,
             });
           });
+          
           res.writeHead(200, {'Content-Type': file.contentType });
           stream.pipe(res);
         } else {
@@ -34,7 +47,9 @@ router.get('/:id', function(req, res, next) {
           res.type('txt').send('Not found');
         }
       });
+      
     }
+    
   });
 });
 
