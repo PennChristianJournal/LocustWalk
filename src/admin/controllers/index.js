c952154380c620733ab57eaf4c4eb8c04a388378
tsuny1 <tsuny@seas.upenn.edu>
2017-07-18 22:42:35 -0400

    created the routing for fetching static content

--- src/admin/controllers/index.js
+++ src/admin/controllers/index.js
@@ -4,6 +4,7 @@ import { Router } from 'express';
 import ViewEngine from '~/viewEngine';
 import Renderer from '~/server/renderer';
 
+import StaticContent from '~/common/models/StaticContent'
 const router = new Router();
 
 import ArticlesController from './articles';
@@ -14,8 +15,83 @@ const views = ViewEngine.getViews('admin');
 Object.keys(views).forEach(key => {
   const view = views[key];
   router.get(view.route, (req, res) => {
+
+
+
     Renderer.render(req, res, view);
   });
 });
 
+
+import formidable from 'express-formidable';
+router.use(formidable());
+
+router.post('/', function(req,res){
+console.log(req.fields);
+var currentPage =  new StaticContent({
+  name: req.fields.name,
+  contentMD: req.fields.markdown,
+  contentHTML: req.fields.html
+});
+var query = {name: req.fields.name};
+
+// StaticContent.findOneAndUpdate(query, currentPage, {new:true, upsert:true}, function(err, doc){
+//
+//     return res.redirect("back");
+// });
+
+
+StaticContent.find({name: req.fields.name}, function(err, docs) {
+
+    if(docs) {
+        StaticContent.find({ name:req.fields.name }).remove().exec();
+
+}
+        var staticPage = new StaticContent(currentPage);
+        staticPage.save(function (err) {
+            if (err) {
+                console.log("error");
+            }
+        })
+
+
+        res.redirect('back');
+  })
+});
+router.get('/deleteData', function(req,res){
+StaticContent.remove({}, function(err, docs){
+  if(err){
+    throw err;
+  }
+})
+
+})
+
+
+router.get('/getStaticContent/:id', function(req,res){
+  StaticContent.findOne({name: req.params.id}, function(err, docs){
+    if(err){
+      throw err;
+    }
+    res.send(docs);
+  })
+      })
+
+
+
+
+
+router.get('/data', function(req,res){
+
+
+var allData = {};
+  StaticContent.find({}, function(err, docs) {
+    if (!err){
+        allData = docs;
+        res.send(allData);
+
+    } else {throw err;}
+});
+
+});
 export default router;
