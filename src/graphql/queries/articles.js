6e75bb4b323175948687c6e919d9a8acc9ef0c98
Austin Eng <213reeses@gmail.com>
2017-07-27 00:38:35 +0000

    Replace article search endpoint with graphql resolver

--- src/graphql/queries/articles.js
+++ src/graphql/queries/articles.js
@@ -16,7 +16,7 @@ import mongoose from 'mongoose';
 import {
   removeEmpty,
   skipLimitArgs,
-  applySkipLimit, 
+  applySkipLimit,
   authenticatedField,
 } from '../helpers';
 
@@ -44,14 +44,14 @@ export const article = {
     if (!(idOrSlug || _id || slug)) {
       return Promise.reject('No id or slug provided');
     }
-    
+
     let field = (_id || mongoose.Types.ObjectId.isValid(idOrSlug)) ? '_id' : 'slug';
-    
+
     let q = Article.findOne(removeEmpty({
       is_published: authenticatedField(context, is_published, true),
       [field]: _id || slug || idOrSlug,
     }), getArticleProjection(fieldASTs));
-    
+
     return q.exec();
   },
 };
@@ -72,12 +72,12 @@ export const articleResponses = {
     if (!parent) {
       return Promise.resolve([]);
     }
-    
+
     let q = Article.find(removeEmpty({
       is_published: authenticatedField(context, is_published, true),
       parent,
     }), getArticleProjection(fieldASTs));
-    
+
     q.sort({ date: -1 });
     q = applySkipLimit(q, skip, limit);
     return q.exec();
@@ -100,12 +100,12 @@ export const articleResponsesCount = {
     if (!parent) {
       return Promise.resolve(0);
     }
-    
+
     let q = Article.count(removeEmpty({
       is_published: authenticatedField(context, is_published, true),
       parent,
     }), getArticleProjection(fieldASTs));
-    
+
     return q.exec();
   },
 };
@@ -123,7 +123,7 @@ export const featuredArticles = {
       is_published: authenticatedField(context, is_published, true),
       is_featured: true,
     }), getArticleProjection(fieldASTs));
-    
+
     q.sort({ date: -1 });
     q = applySkipLimit(q, skip, limit);
     return q.exec();
@@ -142,7 +142,7 @@ export const recentArticles = {
     let q = Article.find(removeEmpty({
       is_published: authenticatedField(context, is_published, true),
     }), getArticleProjection(fieldASTs));
-    
+
     q.sort({ date: -1 });
     q = applySkipLimit(q, skip, limit);
     return q.exec();
@@ -161,5 +161,27 @@ export const articleCount = {
     return Article.count(removeEmpty({
       is_published: authenticatedField(context, is_published, true),
     })).exec();
+  },
+};
+
+export const searchArticles = {
+  type: new GraphQLList(ArticleType),
+  args: Object.assign({
+    title: {
+      name: 'title',
+      type: new GraphQLNonNull(GraphQLString),
+    },
+  }, skipLimitArgs),
+  resolve: (root, {title, skip, limit}, context, fieldASTs) => {
+    if (!context.isAuthenticated()) {
+      return Promise.reject('Not authenticated');
+    }
+    let q = Article.find({
+      title: {
+        $regex: new RegExp(`^${title.toLowerCase()}`, 'i'),
+      },
+    }, getArticleProjection(fieldASTs));
+    q = applySkipLimit(q, skip, limit);
+    return q.exec();
   },
 };
