2bf57204c474b0c9034aff55d109685019a9d83b
Austin Eng <213reeses@gmail.com>
2017-08-09 02:44:50 +0000

    Initialize admin page for features

---
+++ src/admin/frontend/views/features/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import React, {Component} from 'react';
+import AdminLayout from '~/admin/frontend/templates/admin-layout';
+
+export default class FeatureList extends Component {
+  render() {
+    return (
+      <AdminLayout id="admin-page">
+      </AdminLayout>
+    );
+  }
+}
