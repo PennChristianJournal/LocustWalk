4431a0f96dc27d0071c84331d64b15cb049e1428
Austin Eng <213reeses@gmail.com>
2017-08-28 17:57:05 +0000

    Make features resortable

--- src/admin/frontend/views/features/index.js
+++ src/admin/frontend/views/features/index.js
@@ -1,17 +1,28 @@
 'use strict';
 
 import React, {Component} from 'react';
-import {graphql, gql} from 'react-apollo';
+import {compose, graphql, gql} from 'react-apollo';
+import {debounce} from 'underscore';
 import {headData} from '~/common/frontend/head';
 import AdminLayout from '~/admin/frontend/templates/admin-layout';
 import Table from '~/admin/frontend/components/table';
 import Modal from '~/admin/frontend/components/modal';
 import FeatureEdit from '~/admin/frontend/components/feature-edit';
 import FeatureEditPanel from '~/admin/frontend/components/feature-edit-panel';
+import {FEATURE_UPDATE} from '~/admin/frontend/gql/mutations';
 
 class FeatureList extends Component {
   render() {
-    const {features, setFeature} = this.props;
+    const {features, setFeature, swapIndices} = this.props;
+
+    function swap(event, src, tgt) {
+      event.stopPropagation();
+      if (tgt < 0 || tgt >= features.length) {
+        return;
+      }
+      swapIndices(src, tgt);
+    }
+
     return (
       <Table className="table table-striped" head={
         <tr>
@@ -22,8 +33,12 @@ class FeatureList extends Component {
       }>
         {features.map((feature, i) => {
           return (
-            <tr key={i} onClick={() => setFeature(feature) }>
-              <td>{feature.index}</td>
+            <tr key={i + feature._id} onClick={() => setFeature(feature) }>
+              <td>
+                <a href="#" onClick={e => swap(e, i, i - 1)}><i className="fa fa-chevron-up"/></a>&nbsp;
+                {feature.index}&nbsp;
+                <a href="#" onClick={e => swap(e, i, i + 1)}><i className="fa fa-chevron-down"/></a>
+              </td>
               <td>{feature.title}</td>
               <td>{feature.is_published ? <i className="fa fa-check" /> : null}</td>
             </tr>
@@ -46,40 +61,80 @@ const FEATURES_QUERY = gql`
   }
 `;
 
-const FeatureListWithData = graphql(FEATURES_QUERY, {
-  options: {
-    variables: {
-      skip: 0,
+const FeatureListWithData = compose(
+  graphql(FEATURES_QUERY, {
+    options: {
+      variables: {
+        skip: 0,
+      },
     },
-  },
-  props({ data: {loading, features, featureCount, fetchMore}}) {
-    features = features || [];
-    return {
-      features,
-      hasMore() {
-        return features.length < featureCount;
-      },
-      loadMore() {
-        if (loading) {
-          return;
-        }
-        return fetchMore({
-          variables: {
-            skip: features.length,
-          },
-          updateQuery: (previousResult, { fetchMoreResult }) => {
-            if (!fetchMoreResult) {
-              return previousResult;
-            }
-            return Object.assign({}, previousResult, {
-              features: [...previousResult.features, ...fetchMoreResult.features],
-            });
-          },
+    props({ data: {refetch, loading, features, featureCount, fetchMore}}) {
+      features = features || [];
+      return {
+        refetch,
+        features,
+        hasMore() {
+          return features.length < featureCount;
+        },
+        loadMore() {
+          if (loading) {
+            return;
+          }
+          return fetchMore({
+            variables: {
+              skip: features.length,
+            },
+            updateQuery: (previousResult, { fetchMoreResult }) => {
+              if (!fetchMoreResult) {
+                return previousResult;
+              }
+              return Object.assign({}, previousResult, {
+                features: [...previousResult.features, ...fetchMoreResult.features],
+              });
+            },
+          });
+        },
+      };
+    },
+  }),
+  graphql(FEATURE_UPDATE, {
+    name: 'updateFeature',
+    props({ownProps: {features, refetch, onRefetch}, updateFeature}) {
+      function refetchFunc() {
+        return refetch().then(() => {
+          if (onRefetch) {
+            return onRefetch();
+          } else {
+            return Promise.resolve();
+          }
         });
-      },
-    };
-  },
-})(FeatureList);
+      }
+
+      return {
+        swapIndices(src, tgt) {
+          Promise.all([
+            updateFeature({
+              variables: {
+                _id: features[src]._id,
+                feature: {
+                  index: features[tgt].index,
+                },
+              },
+            }),
+            updateFeature({
+              variables: {
+                _id: features[tgt]._id,
+                feature: {
+                  index: features[src].index,
+                },
+              },
+            }),
+          ]).then(refetchFunc);
+        },
+      };
+    },
+  }),
+)(FeatureList);
 
 class FeatureListPage extends Component {
   constructor(props) {
@@ -90,10 +145,48 @@ class FeatureListPage extends Component {
     };
   }
 
+  componentDidMount() {
+    const container = this.refs.list;
+    const table = container.childNodes[0];
+    table.addEventListener('mousewheel', debounce((e) => {
+      if (e.deltaY > 0 && table.scrollTop + table.offsetHeight > table.scrollHeight - 50) {
+        this.loadMoreFeatures();
+      }
+    }, 20, true));
+
+    this.loadMoreFeatures();
+  }
+
+  fetchFeatures(cb) {
+    this.refs.articles.getWrappedInstance().fetchMore(cb);
+  }
+
+  hasMoreFeatures() {
+    return this.refs.articles.getWrappedInstance().hasMore();
+  }
+
   setFeature(feature) {
     this.setState({
       feature,
     });
+  }
+
+  loadMoreFeatures() {
+    const container = this.refs.list;
+    const table = container.childNodes[0];
+    const body = table.childNodes[1];
+    const tableBottom = table.offsetTop + table.offsetHeight;
+    const bodyBottom = body.offsetTop + body.offsetHeight;
+
+    const atBottom = table.scrollTop + table.offsetHeight > table.scrollHeight - 50;
+    const bodyShort = bodyBottom < tableBottom;
+
+    if ((bodyShort || atBottom) && this.refs.featureList.renderedElement.props.hasMore()) {
+      let loaded = this.refs.featureList.renderedElement.props.loadMore();
+      if (loaded) {
+        loaded.then(this.loadMoreFeatures.bind(this));
+      }
+    }
   }
 
   render() {
@@ -150,7 +243,7 @@ class FeatureListPage extends Component {
                     </h1>
                 </div>
                 <div className="admin-list-content" ref="list">
-                  <FeatureListWithData ref="featureList" setFeature={this.setFeature.bind(this)} />
+                  <FeatureListWithData ref="featureList" setFeature={this.setFeature.bind(this)} onRefetch={this.loadMoreFeatures.bind(this)} />
                 </div>
             </div>
         </div>
